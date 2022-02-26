36489a26607016a3e7b02731864152955f7cc947
Chey777cc <97988840+Chey777cc@users.noreply.github.com>
2022-02-26 09:42:18 +0530

    Update character.ts

--- src/commands/Weeb/character.ts
+++ src/commands/Weeb/character.ts
@@ -1,45 +1,74 @@
-/** @format */
-
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
 import { IParsedArgs, ISimplifiedMessage } from "../../typings";
-import { Character } from "@shineiichijo/marika";
+import axios from "axios";
+import request from "../../lib/request";
 import { MessageType } from "@adiwajshing/baileys";
 
 export default class Command extends BaseCommand {
-  constructor(client: WAClient, handler: MessageHandler) {
-    super(client, handler, {
-      command: "character",
-      description: `Searches the given character.`,
-      aliases: ["chara"],
-      category: "weeb",
-      usage: `${client.config.prefix}chara [name]`,
-      baseXp: 50,
-    });
-  }
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "character",
+			description: `Searches the given character.`,
+			aliases: ["chara"],
+			category: "weeb",
+			usage: `${client.config.prefix}chara [name]`,
+			baseXp: 50,
+		});
+	}
 
-  run = async (
-    M: ISimplifiedMessage,
-    { joined }: IParsedArgs
-  ): Promise<void> => {
-    /*eslint-disable @typescript-eslint/no-explicit-any*/
-    /*eslint-disable @typescript-eslint/no-unused-vars*/
-    if (!joined) return void (await M.reply(`Give me a character name, Baka!`));
-    const chitoge = joined.trim();
-    const client = new Character();
-    let chara;
-    try {
-      chara = await client.searchCharacter(chitoge, { page: 1, limit: 10 });
-    } catch (error) {
-      return void M.reply("Couldn't find any matching character");
-    }
-    let text = "";
-    for (const Chara of chara.data) {
-      text += `💙 *Name: ${Chara.name}*\n`;
-      text += `🌐 *URL: ${Chara.url}*\n\n`;
-      text += `Use ${this.client.config.prefix}charaid ${Chara.mal_id} to get the full info of this character.\n\n`;
-    }
-    return void M.reply(await this.client.getBuffer(chara.data[0].images.jpg.image_url), MessageType.image, undefined, undefined, text)
-  };
+	run = async (
+		M: ISimplifiedMessage,
+		{ joined }: IParsedArgs
+	): Promise<void> => {
+		/*eslint-disable @typescript-eslint/no-explicit-any*/
+		/*eslint-disable @typescript-eslint/no-unused-vars*/
+		if (!joined) return void (await M.reply(`Give me a character name, Baka!`));
+		const chitoge = joined.trim();
+		const chara = await axios
+			.get(`https://api.jikan.moe/v3/search/character?q=${chitoge}`)
+			.catch((err: any) => {
+				return void M.reply(`Couldn't find any matching character.`);
+			});
+
+		let text = "";
+		for (let i = 0; i < 10; i++) {
+			text += `❣️ *Name: ${chara?.data.results[i].name}*\n`;
+			text += `🌐 *URL: ${chara?.data.results[i].url}*\n\n`;
+			text += `Use ${this.client.config.prefix}charaid ${chara?.data.results[i].mal_id} to get the full info of this character.\n\n`;
+		}
+
+		const buffer = await request
+			.buffer(chara?.data.results[0].image_url)
+			.catch((e) => {
+				return void M.reply(e.message);
+			});
+		while (true) {
+			try {
+				M.reply(
+					buffer || "✖ An error occurred. Please try again later.",
+					MessageType.image,
+					undefined,
+					undefined,
+					`${text}`,
+					undefined
+				).catch((e) => {
+					console.log(
+						`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`
+					);
+					// console.log('Failed')
+					M.reply(`✖ An error occurred. Please try again later.`);
+				});
+				break;
+			} catch (e) {
+				// console.log('Failed2')
+				M.reply(`✖ An error occurred. Please try again later.`);
+				console.log(
+					`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`
+				);
+			}
+		}
+		return void null;
+	};
 }
